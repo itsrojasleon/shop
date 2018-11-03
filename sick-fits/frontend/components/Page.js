3009d58da467edec91a6b736d1907c0abc12d846
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-11-02 18:03:51 -0600

    Themes and Layout with styled-components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -2,22 +2,39 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
 
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
-const MyButton = styled.button`
-  background: red;
-  font-size: white;
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '1000px',
+  bs: '0 12px 24px 0 rgba(0,0,0,0,0.09)'
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
 `;
-
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+ 
 class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        <MyButton>Click me</MyButton>
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>
+            {this.props.children}
+          </Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
